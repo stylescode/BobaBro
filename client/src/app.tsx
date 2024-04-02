1104e8bd65d4318e2f07c4d757c262ea841a8d32
Styles <stylesk11@gmail.com>
2024-04-02 16:04:10 -0700

    render page based on state

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -7,15 +7,30 @@ import Profile from './profile/profile';
 
 const App: React.FC = () => {
 
+  type Page = 'main' | 'business' | 'profile';
+  const [currentPage, setCurrentPage] = useState('main');
 
+  const changePage = (page: Page) => {
+    setCurrentPage(page);
+  };
 
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'main':
+        return <Main />;
+      case 'business':
+        return <Business changePage={changePage} />;
+      case 'profile':
+        return <Profile />;
+      default:
+        return <Main />;
+    }
+  }
 
   return (
     <div id="app-container">
       <Header />
-      <Main />
-      <Business />
-      <Profile />
+      {renderPage()}
     </div>
   );
 };
