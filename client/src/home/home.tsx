2690b0e28eb4c39df9e8a20b44814d0e3e96cb92
Styles <stylesk11@gmail.com>
2024-04-03 15:55:54 -0700

    track location in state and update after search

--- client/src/home/home.tsx
+++ client/src/home/home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { HttpStatusCode } from 'axios';
 import Search from './search';
 import BusinessList from './businessList';
 
@@ -11,39 +11,55 @@ interface HomeProps {
 
 const Home = ({ changePage }: HomeProps) => {
 
+  const [location, setLocation] = useState({ lat: 33.63875, lng: -117.68577 });
   const [bobaShops, setBobaShops] = useState([]);
 
   useEffect(() => {
-    const script = document.createElement("script");
+    const script: HTMLScriptElement = document.createElement("script");
     script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-NH550YmgcvXuEu4UImnGeBRYiS2jzw&libraries=places`;
     script.onload = () => {
       initMap();
     }
     document.head.appendChild(script);
     return () => {
+      console.log('unmounting', script);
       document.head.removeChild(script);
     }
-  }, []);
+  }, [location]);
 
   function initMap() {
     const service = new google.maps.places.PlacesService(document.createElement('div'));
 
     const searchParams = {
-      location: { lat: 33.63875, lng: -117.68577},
+      location: location,
       rankBy: google.maps.places.RankBy.DISTANCE,
       keyword: "boba bubble tea milk tea"
     }
 
-    service.nearbySearch(searchParams, (results, status) => {
+    service.nearbySearch(searchParams, (results: any, status: HttpStatusCode) => {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         setBobaShops(results);
       }
     });
   }
 
+  function changeLocation(newLocation: any) {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: newLocation }, (results: any, status: HttpStatusCode) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        newLocation = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng()
+        }
+        console.log(newLocation);
+        setLocation(newLocation);
+      }
+    });
+  }
+
   return (
     <div id="home-page">
-      <Search />
+      <Search changeLocation={changeLocation}/>
       <BusinessList bobaShops={bobaShops} changePage={changePage}/>
     </div>
   );
