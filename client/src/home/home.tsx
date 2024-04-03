f2c5f13c9b9588696ff11e187bce34943e0a8a64
Styles <stylesk11@gmail.com>
2024-04-03 12:31:35 -0700

    implement google api to retrieve list of shops

--- client/src/home/home.tsx
+++ client/src/home/home.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Search from './search';
+import BusinessList from './businessList';
 
-const Home = () => {
+declare var google: any;
 
+interface HomeProps {
+  changePage: Function;
+}
 
+const Home = ({ changePage }: HomeProps) => {
 
+  const [bobaShops, setBobaShops] = useState([]);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-NH550YmgcvXuEu4UImnGeBRYiS2jzw&libraries=places`;
+    script.onload = () => {
+      initMap();
+    }
+    document.head.appendChild(script);
+    return () => {
+      document.head.removeChild(script);
+    }
+  }, []);
+
+  function initMap() {
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+
+    const searchParams = {
+      location: { lat: 33.63875, lng: -117.68577},
+      rankBy: google.maps.places.RankBy.DISTANCE,
+      keyword: "boba bubble tea milk tea"
+    }
+
+    service.nearbySearch(searchParams, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        setBobaShops(results);
+      }
+    });
+  }
 
   return (
     <div id="home-page">
-      HOME PAGE
       <Search />
+      <BusinessList bobaShops={bobaShops} changePage={changePage}/>
     </div>
   );
 }
