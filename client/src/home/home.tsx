e98991b795dcad76f1a35b51fc739ddb928dabe5
Styles <stylesk11@gmail.com>
2024-04-03 23:29:05 -0700

    render shops on mount + update location function

--- client/src/home/home.tsx
+++ client/src/home/home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios, { HttpStatusCode } from 'axios';
+import axios from 'axios';
 import Search from './search';
 import BusinessList from './businessList';
 
@@ -11,50 +11,25 @@ interface HomeProps {
 
 const Home = ({ changePage }: HomeProps) => {
 
-  const [location, setLocation] = useState({ lat: 33.63875, lng: -117.68577 });
+  const [location, setLocation] = useState({ lat: 33.64552, lng: -117.84156 });
   const [bobaShops, setBobaShops] = useState([]);
 
   useEffect(() => {
-    const script: HTMLScriptElement = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-NH550YmgcvXuEu4UImnGeBRYiS2jzw&libraries=places`;
-    script.onload = () => {
-      initMap();
-    }
-    document.head.appendChild(script);
-    return () => {
-      console.log('unmounting', script);
-      document.head.removeChild(script);
-    }
+    axios.get('/shops', {
+      params: {
+        location: location,
+        keyword: 'boba bubble tea milk tea'
+      }})
+      .then((response) => {
+        setBobaShops(response.data.results);
+      });
   }, [location]);
 
-  function initMap() {
-    const service = new google.maps.places.PlacesService(document.createElement('div'));
-
-    const searchParams = {
-      location: location,
-      rankBy: google.maps.places.RankBy.DISTANCE,
-      keyword: "boba bubble tea milk tea"
-    }
-
-    service.nearbySearch(searchParams, (results: any, status: HttpStatusCode) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        setBobaShops(results);
-      }
-    });
-  }
-
-  function changeLocation(newLocation: any) {
-    let geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ address: newLocation }, (results: any, status: HttpStatusCode) => {
-      if (status === google.maps.GeocoderStatus.OK) {
-        newLocation = {
-          lat: results[0].geometry.location.lat(),
-          lng: results[0].geometry.location.lng()
-        }
-        console.log(newLocation);
-        setLocation(newLocation);
-      }
-    });
+  function changeLocation(newLocation: string) {
+    axios.get('/geocode', { params: { newLocation }})
+      .then((response) => {
+        setLocation(response.data.results[0].geometry.location);
+      });
   }
 
   return (
