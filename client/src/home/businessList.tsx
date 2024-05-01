1b1d7c36cc7cc4d473a195c99b69f02d149c0787
Styles <stylesk11@gmail.com>
2024-04-30 23:25:19 -0700

    add photo section to cards

--- client/src/home/businessList.tsx
+++ client/src/home/businessList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './businessList.css';
 
 interface BusinessListProps {
   bobaShops: any[];
@@ -18,8 +19,11 @@ const BusinessList = ({ bobaShops, changePage }: BusinessListProps) => {
       return bobaShops.map((shop, index) => {
         return (
           <div key={index} className="listItem" onClick={() => changePage('business', bobaShops[index])}>
-            <h3>{shop.name}</h3>
-            <p>{shop.vicinity}</p>
+            <div className="itemInfo">
+              <h3>{shop.name}</h3>
+              <p>{shop.vicinity}</p>
+            </div>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg" alt="no image available" className="itemKeyPhoto"/>
           </div>
         );
       });
@@ -28,7 +32,6 @@ const BusinessList = ({ bobaShops, changePage }: BusinessListProps) => {
 
   return (
     <div id="business-list">
-      LOCATIONS:
       {renderShops()}
     </div>
   );
