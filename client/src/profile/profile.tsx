b92ffbf4512d63ab615f67309d2070089908620d
Styles <stylesk11@gmail.com>
2024-05-03 16:05:25 -0700

    add categories for profile

--- client/src/profile/profile.tsx
+++ client/src/profile/profile.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BusinessList from '../home/businessList';
+import './profile.css';
 
-const Profile = () => {
+interface ProfileProps {
+  changePage: Function;
+}
 
+const Profile = ({ changePage } : ProfileProps) => {
+
+  const [favShops, setFavShops] = useState([]);
+
+  useEffect(() => {
+    const shops = localStorage.getItem('favShops');
+    if (shops) {
+      setFavShops(JSON.parse(shops));
+    }
+  }, []);
 
   return (
-    <div>PROFILE PAGE</div>
+    <div id="profile-container">
+      <img className="profile-image" src="https://img.freepik.com/free-vector/bubble-tea-sticker-illustrated-blue-background-vector_53876-165070.jpg?w=826&t=st=1712250908~exp=1712251508~hmac=4dd736de2b7518e72fd57baed574f8298f3dab2d6abf49ec3e274e610b0c2170" alt="profile" />
+      <div>TeaDude99</div>
+      <div>Favorite Flavors:</div>
+      <div>Favorite Shops:</div>
+      <BusinessList bobaShops={favShops} changePage={changePage}/>
+      <div>TeaDude99's Ratings</div>
+    </div>
   );
 }
 
