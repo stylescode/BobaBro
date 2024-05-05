ca910dd91be0869ee2629b51dac5c7a24e2027ca
Styles <stylesk11@gmail.com>
2024-05-05 12:00:22 -0700

    add about with img attribution

---
+++ client/src/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+
+  return (
+    <div id="about-container">
+      <div>ABOUT PAGE</div>
+      <div>ATTRIBUTION</div>
+      <div><a href="https://www.freepik.com/free-vector/bubble-tea-sticker-illustrated-blue-background-vector_25522607.htm#fromView=search&page=1&position=46&uuid=9441370f-63f1-4e3f-92f6-b82d0759a3ec">Default User Image by rawpixel.com on Freepik</a></div>
+    </div>
+  )
+}
+
+export default About;
