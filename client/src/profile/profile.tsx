e1437c110083afb6abf506035902f09da5e24b82
Styles <stylesk11@gmail.com>
2024-05-07 14:21:18 -0700

    stub edit function

--- client/src/profile/profile.tsx
+++ client/src/profile/profile.tsx
@@ -22,6 +22,7 @@ const Profile = ({ changePage } : ProfileProps) => {
       <img className="profile-image" src="https://img.freepik.com/free-vector/bubble-tea-sticker-illustrated-blue-background-vector_53876-165070.jpg?w=826&t=st=1712250908~exp=1712251508~hmac=4dd736de2b7518e72fd57baed574f8298f3dab2d6abf49ec3e274e610b0c2170" alt="profile" />
       <div>TeaDude99</div>
       <div>Favorite Flavors:</div>
+      <div>EDIT</div>
       <div>Favorite Shops:</div>
       <BusinessList bobaShops={favShops} changePage={changePage}/>
       <div>TeaDude99's Ratings</div>
